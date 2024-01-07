f39193073b67c1e4a589d4618ff413801fc4696b
myhre062 <myhre062@umn.edu>
2024-01-07 11:16:53 -0600

    completed module 3
    Python files work and produce valid results. May need to come back and fix file paths to make sure I get 100%.

---
+++ python-challenge/PyBank/main.py
@@ -0,0 +1,78 @@
+# import OS module
+import os
+
+# Module for reading CSV files
+import csv
+
+# Define file paths
+csv_resource_path = '/Users/ezrellemyhre-hager/Documents/GitHub/python-challenge/python-challenge/PyBank/Resources/budget_data.csv'
+txt_analysis_path = '/Users/ezrellemyhre-hager/Documents/GitHub/python-challenge/python-challenge/PyBank/analysis/financial_analysis.txt'
+
+# Initialize variables
+total_months = 0
+total_revenue = 0
+dates = []
+revenue = []
+changes_per_month = [] # List to store the changes in revenue per month
+dates_tracker = [] # List to track the corresponding dates for the changes
+
+# Function to calculate the average change in revenue
+def calculate_average_change(revenue):
+
+    for i in range(len(revenue)-1):
+        changes_per_month.append(revenue[i+1] - revenue[i])
+        # Keep track of that dates as you record the changes
+        dates_tracker.append(dates[i+1])
+
+    return sum(changes_per_month) / len(changes_per_month)
+
+# Open the CSV file
+with open(csv_resource_path) as csv_file:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csv_reader = csv.reader(csv_file, delimiter = ',')
+
+    # Skip the header row 
+    next(csv_reader)
+    
+    # Read each row of data after the header
+    for row in csv_reader:        
+        # add to the list of dates
+        dates.append(row[0])
+
+        # add to the list of revenue
+        revenue.append(int(row[1]))
+
+# Calculate total months and total revenue
+total_months = len(dates)
+total_revenue = sum(revenue)
+
+# Calculate average change using the defined function
+average_change = round(calculate_average_change(revenue), 2)
+
+# Find the greatest increase and decrease in revenue
+greatest_increase = max(changes_per_month)
+greatest_decrease = min(changes_per_month)
+greatest_increase_date = dates_tracker[changes_per_month.index(greatest_increase)]
+greatest_decrease_date = dates_tracker[changes_per_month.index(greatest_decrease)]
+
+# Write the analysis results to a text file
+with open(txt_analysis_path, 'w', newline='') as txt_file:
+    txt_file.write("Financial Analysis\n")
+    txt_file.write("----------------------------\n")
+    txt_file.write(f"Total Months: {total_months}\n")
+    txt_file.write(f"Total: ${total_revenue}\n")
+    txt_file.write(f"Average Change: ${average_change}\n")
+    txt_file.write(f"Greatest Increase in Profits: {greatest_increase_date} (${greatest_increase})\n")
+    txt_file.write(f"Greatest Decrease in Profits: {greatest_decrease_date} (${greatest_decrease})\n")
+
+# Print results to the terminal
+print("")
+print("Financial Analysis")
+print("----------------------------")
+print(f"Total Months: {total_months}")
+print(f"Total: ${total_revenue}")
+print(f"Average Change: ${average_change}")
+print(f"Greatest Increase in Profits: {greatest_increase_date} (${greatest_increase})")
+print(f"Greatest Decrease in Profits: {greatest_decrease_date} (${greatest_decrease})")
+print("")
---
+++ python-challenge/PyPoll/main.py
@@ -0,0 +1,78 @@
+# import OS module
+import os
+
+# Module for reading CSV files
+import csv
+
+# Define file paths
+csv_resource_path = '/Users/ezrellemyhre-hager/Documents/GitHub/python-challenge/python-challenge/PyPoll/Resources/election_data.csv'
+txt_analysis_path = '/Users/ezrellemyhre-hager/Documents/GitHub/python-challenge/python-challenge/PyPoll/analysis/election_results.txt'
+
+# Initialize variables for vote counting
+total_votes = 0
+candidate_1 = "Charles Casper Stockham"
+candidate_1_votes = 0
+candidate_2 = "Diana DeGette"
+candidate_2_votes = 0
+candidate_3 = "Raymon Anthony Doane"
+candidate_3_votes = 0
+
+# Open and read the CSV file
+with open(csv_resource_path) as csv_file:
+    # CSV reader specifies delimiter and variable that holds contents
+    csv_reader = csv.reader(csv_file, delimiter = ',')
+
+     # Skip the header row 
+    next(csv_reader)
+
+    # Read each row of data after the header and just count the data
+    for row in csv_reader:
+        total_votes += 1
+        if (row[2] == candidate_1):
+            candidate_1_votes += 1
+        if (row[2] == candidate_2):
+            candidate_2_votes += 1
+        if (row[2] == candidate_3):
+            candidate_3_votes += 1
+
+# Calculate percentage of votes for each candidate
+canidate_1_votes_percentage = round((candidate_1_votes/total_votes)*100, 3)
+canidate_2_votes_percentage = round((candidate_2_votes/total_votes)*100, 3)
+canidate_3_votes_percentage = round((candidate_3_votes/total_votes)*100, 3)
+
+# Function to determine the election winner
+def determine_election_winner():
+    '''
+    Neat trick if you align the candidates with their vote count you can get the winner
+    string based off the index of the candidates total vote count. 
+    '''
+    candidates = [candidate_1, candidate_2, candidate_3]
+    candidates_votes = [candidate_1_votes, candidate_2_votes, candidate_3_votes]
+    election_winner = candidates[candidates_votes.index(max(candidates_votes))]
+    return f"Winner: {election_winner}"
+
+# Write results to a text file
+with open(txt_analysis_path, 'w', newline='') as txt_file:
+    txt_file.write("Election Results\n")
+    txt_file.write("-------------------------\n")
+    txt_file.write(f"Total Votes: {total_votes}\n")
+    txt_file.write("-------------------------\n")
+    txt_file.write(f"{candidate_1}: {canidate_1_votes_percentage}% ({candidate_1_votes})\n")
+    txt_file.write(f"{candidate_2}: {canidate_2_votes_percentage}% ({candidate_2_votes})\n")
+    txt_file.write(f"{candidate_3}: {canidate_3_votes_percentage}% ({candidate_3_votes})\n")
+    txt_file.write("-------------------------\n")
+    txt_file.write(f"{determine_election_winner()}\n")
+    txt_file.write("-------------------------\n")
+
+# Print results to the terminal
+print("")
+print("Election Results")
+print("-------------------------")
+print(f"Total Votes: {total_votes}")
+print("-------------------------")
+print(f"{candidate_1}: {canidate_1_votes_percentage}% ({candidate_1_votes})")
+print(f"{candidate_2}: {canidate_2_votes_percentage}% ({candidate_2_votes})")
+print(f"{candidate_3}: {canidate_3_votes_percentage}% ({candidate_3_votes})")
+print("-------------------------")
+print(f"{determine_election_winner()}")
+print("-------------------------")
